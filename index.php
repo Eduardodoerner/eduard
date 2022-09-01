00e8da7e44a2b89441f4cbc23e164c3f36eab1c6
Eduardodoerner <110205124+Eduardodoerner@users.noreply.github.com>
2022-09-01 20:54:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -20,10 +20,10 @@
 
     if (isset($_POST[''])) {
         $Value1 = $_POST['op'];
-        setcookie($cookieName1,$cookieValue1,time()+(86400 * 30),"/");
+        setcookie($Name1,$Value1,time()+(86400 * 40),"/");
 
         $Value2 = $_POST['op'];
-        setcookie($cookieName2,$cookieValue2,time()+(86400 * 30),"/");
+        setcookie($Name2,$Value2,time()+(86400 * 40),"/");
 
         $num = "";
 
@@ -33,76 +33,6 @@
 
 <form method="post">
     <center>
-        <input type="text" name="result" value="<?php echo $result?>" readonly>
-        <br>
-        <label>1º valor</label>
-        <br>
-        <input type="text" name="value1" value="<?php echo $result?>" readonly>
-        <br>
-        <br>
-        <label>2º valor</label>
-        <br>
-        <input type="text" name="value2" value="<?php echo $result?>" readonly>
-        <br>
-        <br>
-        <label>Resultado</label>
-        <br>
-        <br>
-        <table border="1">
-            <tr>
-                <td>
-                    <input type="submit" name="op" value="/">
-                </td>
-                <td>
-                    <input type="submit" name="submit" value="7">
-                </td>
-                <td>
-                    <input type="submit" name="submit" value="8">
-                </td>
-                <td>
-                    <input type="submit" name="submit" value="9">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="submit" name="submit" value="4">
-                </td>
-                <td>
-                    <input type="submit" name="submit" value="5">
-                </td>
-                <td>
-                    <input type="submit" name="submit" value="6">
-                </td>
-                <td>
-                    <input type="submit" name="op" value="*">
-                </td>
-            </tr>
-            <tr>
-               <td>
-                    <input type="submit" name="op" value="-">
-                </td>
-                <td>
-                    <input type="submit" name="submit" value="1">
-                </td>
-                <td>
-                    <input type="submit" name="submit" value="2">
-                </td>
-                <td>
-                    <input type="submit" name="submit" value="3">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="submit" name="op" value="+">
-                </td>
-                <td>
-                    <input type="submit" name="submit" value="0">
-                </td>
-                <td colspan="2">
-                    <input type="submit" name="equals_to" value="=">
-                </td>
-                
-            </tr>
-        </table>
+       
     </center>
 </form>
